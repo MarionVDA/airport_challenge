08cb87b50515031cd350b48914569ad35b370e63
Marion <marion.vagnerdamicourt@kpmg.co.uk>
2019-09-23 12:45:17 +0100

    third commit

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,25 +6,34 @@ require 'plane'
 
 describe Plane do
   subject(:plane) { Plane.new }
-  describe 'plane to land'
-  it { is_expected.to respond_to :land }
+  describe 'plane to land' do
+    it { is_expected.to respond_to :land }
+  end
+end
 
+describe Plane do
+  subject(:plane) { Plane.new }
   before do
     allow(plane).to receive(:bad_weather).and_return false
   end
-  
   it 'plane to land at an airport' do
     plane.land(plane)
     expect(plane.airport).to include(plane)
   end
-
+end
   # As an air traffic controller
   # So I can get passengers on the way to their destination
   # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
-  describe 'plane to take off'
-  it { is_expected.to respond_to :take_off }
+describe Plane do
+  subject(:plane) { Plane.new }
+  describe 'plane to take off' do
+    it { is_expected.to respond_to :take_off }
+  end
+end
 
+describe Plane do
+  subject(:plane) { Plane.new }
   before do
     allow(plane).to receive(:bad_weather).and_return false
   end
@@ -32,37 +41,45 @@ describe Plane do
     plane.take_off
     expect(plane.airport).not_to include(plane)
   end
+end
 
   # As an air traffic controller
   # To ensure safety
   # I want to prevent landing when the airport is full
 
+describe Plane do
+  subject(:plane) { Plane.new }
   it 'raise error if airport is full' do
     plane = Plane.new
     plane.airport = ['plane1', 'plane2', 'plane3', 'plane4', 'plane5']
     expect { plane.land(plane) }.to raise_error('Airport full!')
   end
+end
 
   # As an air traffic controller
   # To ensure safety
   # I want to prevent takeoff when weather is stormy
 
+describe Plane do
+  subject(:plane) { Plane.new }
   before do
     allow(plane).to receive(:bad_weather).and_return true
   end
   it 'raise error if weather is stormy' do
     expect { plane.take_off }.to raise_error('Stormy weather!')
   end
+end
 
   # As an air traffic controller
   # To ensure safety
   # I want to prevent landing when weather is stormy
 
+describe Plane do
+  subject(:plane) { Plane.new }
   before do
     allow(plane).to receive(:bad_weather).and_return true
   end
   it 'raise error if weather is stormy' do
     expect { plane.land(plane) }.to raise_error('Stormy weather!')
   end
-
 end
